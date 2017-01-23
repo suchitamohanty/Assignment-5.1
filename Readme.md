728f69db8dc5aaa57d8796255a0f04708834b2ec
Suchita Mohanty <mohanty.suchita@gmail.com>
2017-01-23 08:33:54 +0530

    Declaration of variable using Java Script

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Assignment –Declare variables using JavaScript to store your
+name, age, dateofbirth and placeofbirth. Display the value of
+these variables in console.
---
+++ js/app.js
@@ -0,0 +1,8 @@
+var name = 'Suchita';
+var age = 30;
+var dob = new Date('06-19-1986');
+var pob = 'Odisha';
+console.log('name: ' + name);
+console.log('age: ' + age);
+console.log('dob: ' + dob);
+console.log('pob: ' + pob);
